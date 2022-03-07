54ead41b94d27f38a863ca389a703f073a08d6da
Chandan Kumar <chandan@kaushalaminc.com>
2022-03-07 12:47:20 +0530

    some changes inside product modals , schema

--- app/models/products.model.js
+++ app/models/products.model.js
@@ -7,9 +7,12 @@ const ProductsSchema = mongoose.Schema({
   image: { type: String },
   colors: { type: Array },
   company: { type: String },
-  description:
-    "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
-  category: "office",
+  description: {
+    type: String,
+  },
+  category: {
+    type: String,
+  },
 });
 
 module.exports = mongoose.model("Product", ProductsSchema);
